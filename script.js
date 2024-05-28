a8e2351fa6eabcbd04f363c99275c9a1a109ca39
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-05-28 16:34:52 +0300

    compressed preview image

--- script.js
+++ script.js
@@ -6,90 +6,89 @@ const nextButton = document.querySelector(".nextButton");
 const PrevButton = document.querySelector(".prevButton");
 const randomButton = document.querySelector(".randomButton");
 
-
-
-
 // Variables
 const API_KEY = "S1tWDRgN45RVT6Am1EuRaNFCpVFGOVooMKhbkiEhKPSW1P04hSHiUACL";
 const basedUrl = `https://api.pexels.com/v1/search?query=nature&per_page=500`;
 let currentImg = 0;
 
-
-
-
+// Functions
 // Get images data function
-(async function getData() {
-  const res = await fetch(basedUrl, {
+async function getImagesData() {
+  const headers = {
+    Accept: "application/json",
+    Authorization: API_KEY,
+  };
+  const fetchOptions = {
     method: "GET",
-    headers: {
-      Accept: "application/json",
-      Authorization: API_KEY,
-    },
-  });
-  const data = await res.json();
-  addImages(data.photos);
-})();
+    headers,
+  };
 
-
-
+  try {
+    const res = await fetch(basedUrl, fetchOptions);
+    const data = await res.json();
+    renderImages(data.photos);
+  } catch (e) {
+    console.log("Error while fetching images", e);
+  }
+}
+getImagesData();
 
 // Add images to the DOM
-function addImages(data) {
-  // Add images to the page
+function renderImages(data) {
+  let limited = 5
   for (const obj of data) {
-    let urlImg = obj.src.landscape;
+    if (limited > 0) {
+      let urlImg = obj.src.large;
 
-    // slide container
-    const slide = document.createElement("div");
-    slide.classList.add("slide");
-    slidesContainer.appendChild(slide);
-
-    // image
-    const img = document.createElement("img");
-    img.setAttribute("alt", "pexels Image");
-    img.src = urlImg;
-    slide.appendChild(img);
+      const imgSlideHTML = `
+      <div class="slide">
+        <img src="${urlImg}" alt="pexels Image" />
+      </div>`;
+  
+      slidesContainer.innerHTML += imgSlideHTML;
+    }
+    limited--
   }
-  setUpImages()
+
+  handleSlider();
 }
-
-
-
 
 // Set up logic for images
-function setUpImages() {
+function handleSlider() {
   const sliderImgs = document.querySelectorAll(".slides .slide img");
-  // Set first img url
-  sliderImgs[0].src = 'https://www.travelandleisure.com/thmb/KLPvXakEKLGE5AY2jVyovl3Md1k=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/iceland-BEAUTCONT1021-b1aeafa7ac2847a484cbca48d3172b6c.jpg'
-  
+  sliderImgs[0].parentElement.remove();
+
   // Give every img container left position
   sliderImgs.forEach((img, i) => {
-    img.parentElement.style.left = `${(i) * 100}%`;
+    img.parentElement.style.left = `${(i - 1) * 100}%`;
   });
 
-
-
-  function checkSlider(e) {
-    // Change current image depending on clicked button
-    if (e.target.className === "nextButton" && currentImg < sliderImgs.length - 1)         currentImg++;
-    else if (e.target.className === "prevButton" && currentImg !== 0)                      currentImg--;
-    else if (e.target.className === "nextButton" && !(currentImg < sliderImgs.length - 1)) currentImg = 0;
-    else if (e.target.className === "prevButton" && currentImg === 0)                      currentImg = sliderImgs.length - 1;
-
-
-    // Get random img
-    if (e.target.className === "randomButton") {
-      const randomIndexImg = Math.floor(Math.random() * sliderImgs.length);
-      currentImg = randomIndexImg;
-    }
-
-    // Switch to the current img
-    slidesContainer.style.left = `-${(currentImg) * 100}%`;
-  }
-
-
-  nextButton.addEventListener("click", (e) => checkSlider(e));
-  PrevButton.addEventListener("click", (e) => checkSlider(e));
-  randomButton.addEventListener("click", (e) => checkSlider(e));
+  nextButton.addEventListener("click", (e) => checkSlider(e, sliderImgs));
+  PrevButton.addEventListener("click", (e) => checkSlider(e, sliderImgs));
+  randomButton.addEventListener("click", (e) => checkSlider(e, sliderImgs));
 }
 
+function checkSlider(e, slides) {
+  const targetClassName = e.target.className;
+  const isNextButton = targetClassName === "nextButton";
+  const isPrevButton = targetClassName === "prevButton";
+  const isRandomButton = targetClassName === "randomButton";
+  const isLastItem = currentImg > slides.length - 1;
+  const isFirstItem = currentImg === 0;
+
+  if (isNextButton && !isLastItem) currentImg++;
+  else if (isPrevButton && !isFirstItem) currentImg--;
+  else if (nextButton && !isLastItem) currentImg = 0;
+  else if (isPrevButton && isFirstItem) currentImg = slides.length - 1;
+
+  if (isRandomButton) {
+    const randomIndexImg = random(slides);
+    currentImg = randomIndexImg;
+  }
+
+  slidesContainer.style.left = `-${currentImg * 100}%`;
+}
+
+function random(items) {
+  return Math.floor(Math.random() * items.length);
+}
